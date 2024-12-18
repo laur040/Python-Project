7b3d77d5f90c15eb9f66d102102c2d0c96266216
laur040 <lauramironov16@gmail.com>
2024-12-18 16:14:11 +0200

    display registers

--- regEdit/backend/routes/get_all_registers.py
+++ regEdit/backend/routes/get_all_registers.py
@@ -5,21 +5,61 @@ register_bp = Blueprint("register", __name__)
 
 def get_subkeys(path):
     subkeys = []
-    return subkeys
+
+    try:
+        top_level_keys = {"HKEY_CLASSES_ROOT": winreg.HKEY_CLASSES_ROOT,
+            "HKEY_CURRENT_USER": winreg.HKEY_CURRENT_USER,
+            "HKEY_LOCAL_MACHINE": winreg.HKEY_LOCAL_MACHINE,
+            "HKEY_USERS": winreg.HKEY_USERS,
+            "HKEY_CURRENT_CONFIG": winreg.HKEY_CURRENT_CONFIG}
+
+        if path in top_level_keys:
+            key = top_level_keys[path]
+            with winreg.OpenKey(key, "") as reg_key:
+                index = 0
+                while True:
+                    try:
+                        subkey = winreg.EnumKey(reg_key, index)
+                        subkeys.append(subkey)
+                        index += 1
+                    except OSError:
+                        break
+        else:
+            if "\\" not in path:
+                return {"error": "True", "message": "expected '\\' in path"}
+
+            key_name, new_path = path.split("\\", 1)
+            key = getattr(winreg, key_name, None)
+            if not key:
+                return {"error": "True", "message": f"invalid key: {key_name}"}
+
+            with winreg.OpenKey(key, new_path) as reg_key:
+                index = 0
+                while True:
+                    try:
+                        subkey = winreg.EnumKey(reg_key, index)
+                        print(subkey)
+                        subkeys.append(subkey)
+                        index += 1
+                    except OSError:
+                        break
+    except Exception as e:
+        return {"error" : "True", "message" : str(e)}
+    return {"error": "False", "subkeys": {key: key for key in subkeys}}
+
+
 
 @register_bp.route("/api/register", methods=["GET"])
-
 def get_registry_subkeys():
     path = request.args.get("path")
     if not path:
-        return jsonify({
-            "HKEY_CLASSES_ROOT": [],
+        return jsonify({"HKEY_CLASSES_ROOT": [],
             "HKEY_CURRENT_USER": [],
             "HKEY_LOCAL_MACHINE": [],
             "HKEY_USERS": [],
-            "HKEY_CURRENT_CONFIG": []
-        })
+            "HKEY_CURRENT_CONFIG": []})
 
-    subkeys = get_subkeys(path)
-
-    return jsonify({path: subkeys})
+    rez = get_subkeys(path)
+    if rez["error"] == "True":
+        return jsonify({"error" : rez["message"]}), 400
+    return jsonify({path: rez["subkeys"]})
