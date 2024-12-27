acfcf246f55f5964cf1cfd0522ab7a8bca2a9d08
laur040 <lauramironov16@gmail.com>
2024-12-27 15:32:05 +0200

    css added for the split screen look

--- regEdit/backend/app.py
+++ regEdit/backend/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, send_from_directory
+from flask import render_template
 from routes.get_all_registers import register_key_bp
 from routes.get_values import register_val_bp
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../frontend/templates", static_folder="../frontend/static")
 app.register_blueprint(register_key_bp)
 app.register_blueprint(register_val_bp)
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return send_from_directory("../frontend", "home.html")
+    return render_template("home.html")
 
 
 
